46c5880068c76340eb0170d73c0436c3b3baeaa5
PPPDUD <107440101+PPPDUD@users.noreply.github.com>
2026-06-26 19:59:48 -0400

    Initial storage development + UI improvements

--- popup.js
+++ popup.js
@@ -1,25 +1,41 @@
-async function register_bonobo(file) {
-    let bonobo_object = JSON.parse(await file.text());
-    console.log(bonobo_object);
+async function register_bonobo (file, is_registered) {
+  let bonobo_object = JSON.parse(await file.text())
+  console.log(bonobo_object)
 
-    chrome.userScripts.register([{
-        id: bonobo_object["id"],
-        matches: bonobo_object["match"],
-        js: [{ code: bonobo_object["code"] }],
-        world: "MAIN"
-    }]);
+  chrome.userScripts.register([
+    {
+      id: bonobo_object['id'],
+      matches: bonobo_object['match'],
+      js: [{ code: bonobo_object['code'] }],
+      world: 'MAIN'
+    }
+  ])
 
-    alert(bonobo_object["id"]);
+  if (!is_registered) {
+    await chrome.storage.local.set({ [bonobo_object['id']]: bonobo_object })
+  }
+
+  actionText.innerText =
+    'Userscript installed at timestamp ' + Math.floor(Date.now() / 1000) + '!'
 }
 
-const fileSelector = document.getElementById('file-selector');
-const removeButton = document.getElementById('remove-button');
-const removeInput = document.getElementById('remove-input');
-fileSelector.addEventListener('change', async (event) => {
-    let file = event.target.files[0];
-    console.log(file);
-    register_bonobo(file);
-    fileSelector.value = null;
-});
+const fileSelector = document.getElementById('file-selector')
+const removeButton = document.getElementById('remove-button')
+const removeInput = document.getElementById('remove-input')
+const actionText = document.getElementById('action-text')
 
-removeButton.addEventListener('click', () => {chrome.userScripts.unregister({ids: [removeInput.value]})})
+fileSelector.addEventListener('change', async event => {
+  let file = event.target.files[0]
+  console.log(file)
+  register_bonobo(file)
+  fileSelector.value = null
+})
+
+removeButton.addEventListener('click', async () => {
+  try {
+    await Promise.all([chrome.userScripts.unregister({ ids: [removeInput.value] }), chrome.storage.local.remove(removeInput.value)]);
+    actionText.innerText = 'Userscript removed at timestamp ' + Math.floor(Date.now() / 1000) + '!'
+  } catch (error) {
+    actionText.innerText = error;
+  }
+})
