d2755a35e685429dbf5d2e1779c5c5f57842ed01
PPPDUD <107440101+PPPDUD@users.noreply.github.com>
2026-06-27 12:07:22 -0400

    Further restructuring, UI improvements

--- popup/main/popup.js
+++ popup/main/popup.js
@@ -1,24 +1,3 @@
-async function register_bonobo (file, is_registered) {
-  let bonobo_object = JSON.parse(await file.text())
-  console.log(bonobo_object)
-
-  chrome.userScripts.register([
-    {
-      id: bonobo_object['id'],
-      matches: bonobo_object['match'],
-      js: [{ code: bonobo_object['code'] }],
-      world: 'MAIN'
-    }
-  ])
-
-  if (!is_registered) {
-    await chrome.storage.local.set({ [bonobo_object['id']]: bonobo_object })
-  }
-
-  actionText.innerText =
-    `Userscript "${bonobo_object["id"]}" installed at timestamp ${Math.floor(Date.now() / 1000)}!`
-}
-
 const fileSelector = document.getElementById('file-selector')
 const removeButton = document.getElementById('remove-button')
 const removeInput = document.getElementById('remove-input')
