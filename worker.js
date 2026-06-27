2fc9e4f77ce11306b7d1499dfc66ce3d38e88eb7
PPPDUD <107440101+PPPDUD@users.noreply.github.com>
2026-06-27 11:52:34 -0400

    Restructuring, import/export screen

--- worker.js
+++ worker.js
@@ -1,3 +1,3 @@
 chrome.action.onClicked.addListener(() => {
-    chrome.windows.create({url: chrome.runtime.getURL('popup/popup.html'), type: "popup", height:500, width:500, focused: true})
+    chrome.windows.create({url: chrome.runtime.getURL('popup/main/index.html'), type: "popup", height:500, width:500, focused: true})
 })
